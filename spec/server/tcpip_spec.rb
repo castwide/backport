ef1531fad1fbdd578ec45c5b2af389f8e411f854
Fred Snyder <fsnyder@castwide.com>
2018-12-20 15:04:42 -0500

    Changed port in Tcpip spec.

--- spec/server/tcpip_spec.rb
+++ spec/server/tcpip_spec.rb
@@ -4,9 +4,9 @@ require 'socket'
 RSpec.describe Backport::Server::Tcpip do
   it "reads input on ticks" do
     input = double(IO, sysread: 'sent', flush: nil)
-    server = Backport::Server::Tcpip.new(host: '127.0.0.1', port: 99999, adapter: InputAdapter)
+    server = Backport::Server::Tcpip.new(host: '127.0.0.1', port: 9999, adapter: InputAdapter)
     server.start
-    client = TCPSocket.new('127.0.0.1', 99999)
+    client = TCPSocket.new('127.0.0.1', 9999)
     client.send "sent", 0
     sleep 0.1
     server.tick
